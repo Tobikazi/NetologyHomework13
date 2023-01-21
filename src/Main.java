8f06c76c0c1014c6035dccffbbe3a6d755d8f883
Tobikazi <117998999+Tobikazi@users.noreply.github.com>
2023-01-21 13:58:00 +0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,56 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    private static final Product[] products = {
+            new Product("Хлеб", 60.0),
+            new Product("Молоко", 80.0),
+            new Product("Яблоки (1 кг.)", 140.0),
+    };
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        Scanner sc = new Scanner(System.in);
+        String s;
+        Basket shoppingCart = new Basket(products);
+        int selectedItem;
+        int itemCount;
+        File basketFile = new File("basket.bin");
+        if (basketFile.exists()) {
+            System.out.println("Перезаписать список?");
+            if (sc.nextLine().equals("")) {
+                shoppingCart = Basket.loadFromBinFile(basketFile);
+            }
+        }
+        while (true) {
+            shoppingCart.printProductsList();
+            s = sc.nextLine();
+            String[] inputValues = s.split(" ");
+            if (inputValues.length == 2) {
+                try {
+                    selectedItem = Integer.parseInt(inputValues[0]);
+                    itemCount = Integer.parseInt(inputValues[1]);
+                    if (selectedItem <= 0 || selectedItem > products.length) {
+                        System.out.print("\nНеправильный номер продукта\n");
+                        continue;
+                    }
+                    if (itemCount <= 0) {
+                        continue;
+                    }
+                    shoppingCart.addToCart(selectedItem - 1, itemCount);
+                    shoppingCart.saveBin(basketFile);
+                } catch (NumberFormatException nfe) {
+                    System.out.println("\nИспользовать нужно целые числа");
+                } catch (FileNotFoundException e) {
+                    throw new RuntimeException(e);
+                }
+            } else if (s.equals("end")) {
+                break;
+            }
+            System.out.println("\n");
+        }
+        sc.close();
+        shoppingCart.printCart();
+    }
+}
